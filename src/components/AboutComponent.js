752937096c6434e8146d48a4ad9f40558cc93614
mattskrobola <mattskrobola@gmail.com>
2019-05-06 18:51:32 -0400

    gh-pages

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -18,7 +18,13 @@ class About extends Component {
         return (
             <div>
                 <Header/>
-                <h1> About this quiz:</h1>
+                <h1> About this quiz: </h1>
+                <h3>If you FEEL first before you communicate, you'll build stronger relationships through all of your
+                    online channels and in-person too. Here are several questions you can ask yourself about Facing
+                    your Fears, communicating with Empathy, using Ethics and good judgment and discovering your Love
+                    for the mission. Modern Communicators are generally taught to consider their audience, timing,
+                    channels, messaging and measurement. However, when you add FEEL to your plan, your communications
+                    will resonate and your relationships will grow stronger with the people that matter.</h3>
                 <img className="pic" src={feel}></img>
                 <Footer/>
             </div>
